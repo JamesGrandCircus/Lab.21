92c87c59634bf4eb9526564cdb6cd201612e7d7e
Zachary Hordynski <zach.hordynski@gmail.com>
2019-01-27 21:07:24 -0500

    Added more validation

--- Lab.21/Models/Register.cs
+++ Lab.21/Models/Register.cs
@@ -20,10 +20,11 @@ namespace Lab._21.Models
         public string LastName { get; set; }
 
         [Required]
-        //[RegularExpression("^ ([a-zA-Z0-9_\-\.] +)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a - zA - Z0 - 9\-]+\.)+))([a - zA - Z]{2,4}|[0-9]{1,3})(\]?)$")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.] +)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a - zA - Z0 - 9\-]+\.)+))([a - zA - Z]{2,4}|[0-9]{1,3})(\]?)$")]
         public string Email { get; set; }
 
         [Required]
+        [RegularExpression(@"^([0-9]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4}|[a-zA-Z0-9]{7})$")]
         public int PhoneNumber { get; set; }
 
         [Required]
