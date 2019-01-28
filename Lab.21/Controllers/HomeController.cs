41e5812ea1e8b40fa4ba448e9b761e342050c19d
Zachary Hordynski <zach.hordynski@gmail.com>
2019-01-27 20:40:09 -0500

    Finished Lab 21

--- Lab.21/Controllers/HomeController.cs
+++ Lab.21/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab._21.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -24,6 +25,34 @@ namespace Lab._21.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Register newRegister)
+        {
+
+            if (ModelState.IsValid)
+            {
+                TempData["UserName"] = newRegister.FirstName;
+                return RedirectToAction("Registered");
+            }
+
+            else
+            {
+                ViewBag.ErrorMessage = "The information you entered is invalid";
+                return RedirectToAction("Register");
+            }
+
+        }
+
+        public ActionResult Registered()
+        {
             return View();
         }
     }
---
+++ Lab.21/Models/Register.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab._21.Models
+{
+    public class Register
+    {
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(15)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(15)]
+        public string LastName { get; set; }
+
+        [Required]
+        //[RegularExpression("^ ([a-zA-Z0-9_\-\.] +)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a - zA - Z0 - 9\-]+\.)+))([a - zA - Z]{2,4}|[0-9]{1,3})(\]?)$")]
+        public string Email { get; set; }
+
+        [Required]
+        public int PhoneNumber { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
